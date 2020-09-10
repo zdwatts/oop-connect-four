67f40fe55e6bfed4ac8b96980605246838d7dc22
kerri <gant.kerri@gmail.com>
2020-09-10 15:43:02 -0400

    Players name input starts game

--- connect-four.js
+++ connect-four.js
@@ -5,8 +5,25 @@ class Game {
 let game = undefined;
 
 window.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("player-1-name")
-    .addEventListener("keyup", () => {
-        
+    // let form = document.getElementById('form-holder')
+    // form.addEventListener("click", event => {
+    // })
+    let playerOne = document.getElementById('player-1-name')
+    let playerTwo = document.getElementById('player-2-name')
+    let newGame = document.getElementById('new-game')
+    playerOne.addEventListener("keyup", () => {
+        playerTwo.addEventListener("keyup", () => {
+            if (playerOne.value && playerTwo.value) {
+                newGame.disabled = false
+            }
+        })
     })
+    // document.getElementById("player-2-name")
+    //         let newGame = document.getElementById('new-game')
+    //         let playerOne = document.getElementById('player-1-name').value
+    //         let playerTwo = document.getElementById('player-2-name').value
+    //         if (playerOne && playerTwo) {
+    //             newGame.disabled = false
+    //         }
+    //     })
 })
