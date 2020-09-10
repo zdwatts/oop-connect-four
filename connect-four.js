ad770fe2e8ef15615fbee52106c9aca2d4038eb9
zacwatts <zdwatts@gmail.com>
2020-09-10 11:53:11 -0600

    Started New Game

---
+++ connect-four.js
@@ -0,0 +1,12 @@
+class Game {
+
+}
+
+let game = undefined;
+
+window.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("player-1-name")
+    .addEventListener("keyup", () => {
+        
+    })
+})
